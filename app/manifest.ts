e40da82cdb3874140c8acd54726413f290146f24
Vkysrn-ux <sales@britexcbe.com>
2025-10-12 22:41:12 +0530

    logo

--- app/manifest.ts
+++ app/manifest.ts
@@ -17,7 +17,21 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/logo.png',
         sizes: '1200x1200',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
+      },
+
+      {
+        src: '/logo-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+
+      {
+        src: '/logo-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any',
       },
     ],
   }
